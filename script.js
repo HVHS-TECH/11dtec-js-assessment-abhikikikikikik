c4409bb5971d1d1521a6851dcbb42b9e473d0fb4
John Doe <johndoe@example.com>
2026-06-26 14:39:18 +1200

    Commit changes

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ Header
 
 // Variables
 
+const welcomeMessage = document.querySelector(".container p");
+let guestName = localStorage.getItem("savedName");
+
 /******************************
  Main code
  ******************************/
+
+if (!guestName) {
+  guestName = prompt("Welcome to Masa & Lime! What is your name?");
+  if (guestName) {
+    localStorage.setItem("savedName", guestName);
+  }
+}
+
+if (guestName) {
+  welcomeMessage.textContent = "Welcome, " + guestName + "!";
+}
 
 /******************************
 Functions
